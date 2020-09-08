682b4fcae1c82ca3d019e74df8825215a102d6c6
macnick <nick@macexperts.gr>
2020-09-08 19:20:12 +0300

    Write first tests for models

--- spec/models/bike_spec.rb
+++ spec/models/bike_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Bike, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:displacement) }
+  it { should validate_presence_of(:power) }
+  it { should validate_presence_of(:torque) }
+  it { should validate_presence_of(:image) }
+  # Association test
+  # ensure ToBikedo model has a 1:m relationship with the Booking model
+  it { should have_many(:bookings).dependent(:destroy) }
 end
--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Booking, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:bike_id) }
+  it { should validate_presence_of(:user_id) }
+  
+  it { should belong_to(:bike) }
+  it { should belong_to(:user) }
 end
