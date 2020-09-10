43654dfa362a48fa0ede466befaac9046f3021a2
macnick <nick@macexperts.gr>
2020-09-10 19:45:36 +0300

    Write tests for users

--- app/controllers/api/v1/bookings_controller.rb
+++ app/controllers/api/v1/bookings_controller.rb
@@ -1,2 +1,41 @@
-class BookingsController < ApplicationController
+class Api::V1::BookingsController < Api::V1::ApiController
+  before_action :set_booking, only: [:show, :update, :destroy]
+
+  def index
+    @bookings = Booking.all
+    json_response(@bookings)
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      json_response(@booking, :created)
+    else
+      json_response(@booking.errors, :unprocessable_entity)
+    end
+  end
+
+  def show
+    json_response(@booking)
+  end
+
+  def update
+    @booking.update(booking_params)
+    head :no_content
+  end
+
+  def destroy
+    @booking.destroy
+  end
+
+  private
+
+  def booking_params
+    # whitelist params
+     params.permit(:bike_id, :user_id)
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,5 @@ class Booking < ApplicationRecord
   belongs_to :bike
   belongs_to :user
 
-  validates_presence_of :bike_id, :user_id
+  validates_presence_of :date, :bike_id, :user_id
 end
