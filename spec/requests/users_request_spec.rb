85f442f9c6182c7b2cbb617f549674448065140a
macnick <nick@macexperts.gr>
2020-09-13 19:14:13 +0300

    Fix rubocop errors

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  let!(:users) { create_list(:user, 4)}
+  let!(:users) { create_list(:user, 4) }
   let(:user_id) { users.first.id }
 
   describe 'GET /users' do
