fe18467117680f2ea4f14f16a7fc4b186af2d96d
macnick <nick@macexperts.gr>
2020-09-18 20:00:22 +0300

    Delete useless user tests

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -4,43 +4,8 @@ RSpec.describe 'Users', type: :request do
   let!(:users) { create_list(:user, 4) }
   let(:user_id) { users.first.id }
 
-  describe 'GET /users' do
-    before {get '/api/v1/users'}
-    it 'returns users' do
-      expect(json).not_to be_empty
-      expect(json.size).to eq(4)
-    end
-
-    it 'returns status code 200' do
-      expect(response).to have_http_status(200)
-    end
-  end
 
   # Test suite for GET /users/:id
-  describe 'GET /users/:id' do
-    before { get "/api/v1/users/#{user_id}" }
+  
 
-    context 'when the record exists' do
-      it 'returns the user' do
-        expect(json).not_to be_empty
-        expect(json['id']).to eq(user_id)
-      end
-
-      it 'returns status code 200' do
-        expect(response).to have_http_status(200)
-      end
-    end
-
-    context 'when the record does not exist' do
-      let(:user_id) { 100 }
-
-      it 'returns status code 404' do
-        expect(response).to have_http_status(404)
-      end
-
-      it 'returns a not found message' do
-        expect(response.body).to match(/Couldn't find User/)
-      end
-    end
-  end
 end
