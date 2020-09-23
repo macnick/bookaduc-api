e25b77ef844c23c181eee2624699b0abfabcd3a8
macnick <nick@macexperts.gr>
2020-09-23 12:36:47 +0300

    Fix rspec User tests

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,16 @@
 class Api::V1::UsersController < ApplicationController
-  # skip_before_action :authorize_request, only: :create
+  skip_before_action :authorize_request, only: :create
 
   def create
     user = User.create!(user_params)
     auth_token = AuthenticateUser.new(user.email, user.password).call
     response = { message: Message.account_created, auth_token: auth_token }
     json_response(response, :created)
+  end
+
+  def index
+    @users = User.all
+    render json: @users, each_serializer: UserSerializer
   end
 
   def show
