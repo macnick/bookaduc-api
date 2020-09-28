cb37a4fe67a3f5caed3cc60a8d095bd46cf85038
macnick <nick@macexperts.gr>
2020-09-28 19:52:59 +0300

    Change password_digest to password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :bikes, through: :bookings
 
-  validates_presence_of :name, :email, :password_digest
+  validates_presence_of :name, :email, :password
   validates :email, uniqueness: true
 end
