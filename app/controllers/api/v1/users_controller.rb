7b9b9af16858332d21dfd624180e50fe3ca067ce
macnick <nick@macexperts.gr>
2020-09-28 20:15:23 +0300

    Fix user_params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -20,11 +20,10 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(
+    params.require(:user).permit(
       :name,
       :email,
       :password,
-      :password_confirmation
     )
   end
 end
